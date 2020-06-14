bec5a473652c6bef15572730cc27ba26f7322ace
Thierno <th.barry@yahoo.com>
2020-06-14 21:14:02 +0200

    added update management

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import { Button, Table,Modal,InputGroup,FormControl } from 'react-bootstrap';
 import Axios from 'axios';
 
 class App extends Component {
+  /** 
+   * First we need to use json-server for API Fake data 
+   *  see https://github.com/typicode/json-server for more detail.
+   * 
+   * We also need reactbootstrap  
+   * see https://react-bootstrap.netlify.app/
+   * 
+  */
   constructor(){
     super();
     this.state = {
@@ -12,25 +20,30 @@ class App extends Component {
         title:'',
         rating:''
       },
-      newBookModal: false
+      editBookData:{
+        id: '',
+        title:'',
+        rating:''
+      },
+      newBookModal: false,
+      editBookModal: false,
     }
   }
-  componentDidMount(){
-    Axios.get('http://localhost:3000/book').then((response)=>{
-       this.setState({
-         books: response.data
-       })
-    });
+  
+  handleClose(){
+    this.setState({
+      newBookModal: false
+    })
   }
- 
- handleClose(){
-  this.setState({
-    newBookModal: false
-  })
- }
+  /** install axios with npm
+   *  add axios command 
+   * 
+  */
+  componentDidMount(){
+   this._refreshBook();
+  }
  createNewBook(){
   Axios.post('http://localhost:3000/book',this.state.newBookData).then((response)=>{
-    console.log(response.data);
     const {books} = this.state;
     books.push(response.data);
     this.setState({
@@ -38,12 +51,56 @@ class App extends Component {
     })
   })
 }
+editBook(id, title, rating){
+  this.setState({
+    editBookData:{id,title,rating},
+    editBookModal:!this.state.editBookModal
+  })
+}
+updateBook(){
+  const { title,rating } = this.state.editBookData ;
+   Axios.put('http://localhost:3000/book/'+this.state.editBookData.id,
+         { 
+           title:title,
+           rating:rating
+         }
+         ).then((response)=>{
+           this._refreshBook();
+           this.setState({
+             editBookModal:false,
+             editBookData: {
+               id:'',
+               title:'',
+               rating:''
+             }
+           })
+         });
+}
  toggleNewBook(){
    this.setState({
      newBookModal: true
    })
  }
- 
+ _refreshBook(){
+  Axios.get('http://localhost:3000/book').then((response)=>{
+    this.setState({
+      books: response.data
+    })
+ });
+ }
+ toggleEditBook(){
+  this.setState({
+    editBookModal: true
+  })
+ }
+ deleteBook(){
+
+ }
+ handleCloseEditBook(){
+  this.setState({
+    editBookModal: !this.state.editBookModal 
+  })
+ }
   render(){
     const { books } = this.state;
     return (
@@ -100,6 +157,63 @@ class App extends Component {
                   </Button>
                 </Modal.Footer>
               </Modal>
+              {/* 
+                 La modification 
+              */}  
+              <Button variant="primary" onClick={this.toggleEditBook.bind(this)} className="">
+                EditeBook
+              </Button>
+
+              <Modal  show={this.state.editBookModal} onHide={this.handleCloseEditBook.bind(this)} animation={false}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Edit book</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <InputGroup size="sm" className="mb-3">
+                      <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-sm" >Title</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl aria-label="title" defaultValue={this.state.editBookData.title}
+                                 onChange={(e)=>
+                                               {
+                                                const { editBookData } = this.state
+                                                editBookData.title = e.target.value;
+                                                this.setState({
+                                                  editBookData:editBookData
+                                                })
+                                               }
+                                         } 
+                                 aria-describedby="inputGroup-sizing-sm" />
+                </InputGroup>
+                <InputGroup size="sm" className="mb-3">
+                      <InputGroup.Prepend>
+                        <InputGroup.Text id="inputGroup-sizing-sm" >Rating</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl aria-label="title"  defaultValue={this.state.editBookData.rating} 
+                                 onChange={(e)=>{
+                                                  const { editBookData } = this.state
+                                                  editBookData.rating = e.target.value;
+                                                  this.setState({
+                                                    editBookData: editBookData
+                                                  })
+                                                }
+                                           } 
+                                 aria-describedby="inputGroup-sizing-sm" />
+                </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                  {/*
+                    - editBook gère la mise à jour du state 
+                    - update gère celle de l'API
+                  */}
+                  <Button variant="primary" onClick={this.updateBook.bind(this)}>
+                    Update 
+                  </Button>
+                  <Button variant="secondary" onClick={this.handleCloseEditBook.bind(this)}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
               <Table>
                 <thead>
                   <tr>
@@ -117,8 +231,8 @@ class App extends Component {
                           <td>{book.title}</td>
                           <td>{book.rating}</td>
                           <td>
-                            <Button variant="success" className="mr-2">Edit</Button>
-                            <Button variant="danger">Delete</Button>
+                            <Button variant="success" className="mr-2" onClick={ this.editBook.bind(this, book.id, book.title, book.rating)}>Edit</Button>
+                            <Button variant="danger" onClick={this.deleteBook.bind(this)}>Delete</Button>
                           </td>
                         </tr>
                     )
